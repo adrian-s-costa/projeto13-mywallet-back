d2479782fb9af4605d8e74a8a017dd7ed4018179
pink_glb <adriancosta1215@gmail.com>
2022-07-04 19:20:48 -0400

    arrumando

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 app.use(authRouter);
-app.use(transRouter)
+app.use(transRouter);
 
 app.listen(process.env.PORT, ()=> {
     console.log("Server running on port " + process.env.PORT)
